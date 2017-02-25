0768896ee8b18caa3350e092372f8f40e2eef8a4
fenrisbear <johnolav.storvold@gmail.com>
2017-02-25 03:22:53 +0100

    alter tablespace

--- src/main/resources/db/migration/V1__Initial_Database_Schemas.sql
+++ src/main/resources/db/migration/V1__Initial_Database_Schemas.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE STOUT;
 
-CREATE TABLE Crawls
+CREATE TABLE STOUT.Crawls
 (
   id SERIAL PRIMARY KEY,
   title VARCHAR(50) NOT NULL,
@@ -13,20 +13,20 @@ CREATE TABLE Crawls
   description VARCHAR(800) NOT NULL
 );
 
-CREATE TABLE CrawlParticipants
+CREATE TABLE STOUT.CrawlParticipants
 (
   crawl_id SERIAL PRIMARY KEY,
   user_id INT NOT NULL,
   status CHAR(1) NOT NULL
 );
 
-CREATE TABLE Routes
+CREATE TABLE STOUT.Routes
 (
   route_id SERIAL PRIMARY KEY,
   owner_id INT NOT NULL
 );
 
-CREATE TABLE Stops
+CREATE TABLE STOUT.Stops
 (
   stop_id SERIAL PRIMARY KEY,
   title VARCHAR(50) NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE Stops
   lng FLOAT
 );
 
-CREATE TABLE RouteStops
+CREATE TABLE STOUT.RouteStops
 (
   route_stop_id SERIAL PRIMARY KEY,
   stop_id INT NOT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE RouteStops
   route_fk INT REFERENCES Routes(route_id)
 );
 
-CREATE TABLE Users
+CREATE TABLE STOUT.Users
 (
   user_id SERIAL PRIMARY KEY,
   username VARCHAR(15) NOT NULL,
@@ -54,7 +54,7 @@ CREATE TABLE Users
   facebook_id INT NOT NULL
 );
 
-CREATE TABLE Wishes
+CREATE TABLE STOUT.Wishes
 (
   wish_id SERIAL PRIMARY KEY,
   user_id INT NOT NULL,
@@ -63,9 +63,9 @@ CREATE TABLE Wishes
   stop_fk INT REFERENCES Stops(stop_id)
 );
 
-CREATE INDEX idx_routes_owner ON Routes(route_id);
-CREATE INDEX idx_crawl_owner ON Crawls(owner_id);
-CREATE INDEX idx_stop_title_lower ON Stops(lower(title));
-CREATE INDEX idx_user_username ON Users(username);
-CREATE INDEX idx_user_email ON Users(email);
-CREATE INDEX idx_wish_user_id ON Wishes(user_id);
+CREATE INDEX idx_routes_owner ON STOUT.Routes(route_id);
+CREATE INDEX idx_crawl_owner ON STOUT.Crawls(owner_id);
+CREATE INDEX idx_stop_title_lower ON STOUT.Stops(lower(title));
+CREATE INDEX idx_user_username ON STOUT.Users(username);
+CREATE INDEX idx_user_email ON STOUT.Users(email);
+CREATE INDEX idx_wish_user_id ON STOUT.Wishes(user_id);
