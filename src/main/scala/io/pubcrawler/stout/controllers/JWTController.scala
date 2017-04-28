d3a6667b2e191735a42e776ea2c2f4d34bde3449
Sindre Magnussen <sindre.magnussen@itera.no>
2017-04-28 23:24:04 +0200

    feat: Add signup endpoint to JWTController

--- src/main/scala/io/pubcrawler/stout/controllers/JWTController.scala
+++ src/main/scala/io/pubcrawler/stout/controllers/JWTController.scala
@@ -1,8 +1,7 @@
 package io.pubcrawler.stout.controllers
 
 import akka.util.Timeout
-import com.typesafe.scalalogging.LazyLogging
-import io.pubcrawler.stout.db.{TableDefinitions, User}
+import io.pubcrawler.stout.db.{Gender, TableDefinitions, User}
 import io.pubcrawler.stout.models.Result
 import io.pubcrawler.stout.util.{JWTUtil, JsonFormat, UtilAsync}
 import org.scalatra.{FutureSupport, ScalatraServlet}
@@ -16,14 +15,43 @@ import scala.concurrent.Future
 import scala.concurrent.duration._
 
 case class Credentials (username: Option[String], password: Option[String])
+case class SignupCredentials(username: Option[String], email: Option[String], password: Option[String], passwordConfirmation: Option[String])
 
 class JWTController(db: Database) extends ScalatraServlet
-  with JacksonJsonSupport with FutureSupport with TableDefinitions with JsonFormat with UtilAsync with LazyLogging with JWTUtil {
+  with JacksonJsonSupport with FutureSupport with TableDefinitions with JsonFormat with UtilAsync with JWTUtil {
   protected implicit val timeout = new Timeout(2.seconds)
 
   before() {
     contentType = formats("json")
   }
+
+  post("/signup") {
+    val json = parse(request.body)
+    val credentials = json.extract[SignupCredentials]
+    credentials match {
+      case SignupCredentials(Some(username), Some(email), Some(passwd), Some(passwdC)) => {
+        val result: Future[Option[User]] = db.run(users.filter(_.username === username).result.headOption)
+        result.map(
+          _.map(
+            u => noAccess("User already exists")
+          ).getOrElse(
+            if(passwd == passwdC) {
+              val insertQuery = users returning users.map(_.id) into ((user, id) => user.copy(id = Some(id)))
+              val insertUser = insertQuery += User(None, username, BCrypt.hashpw(passwd, BCrypt.gensalt()), None, Gender.O, email, -1)
+              db.run(insertUser).map {
+                case res: User => Result(201, "User created: " + res.username)
+                case _ => Result(500, "Server error")
+              }
+            } else {
+              noAccess("Password confirmation invalid")
+            }
+          )
+        )
+      }
+      case _ => Result(404, "not valid endpoint")
+    }
+  }
+
 
   post("/login") {
     val json = parse(request.body)
@@ -47,8 +75,8 @@ class JWTController(db: Database) extends ScalatraServlet
     }
   }
 
-  def noAccess() {
-    Result(403, "No access")
+  def noAccess(reason: String): Result = {
+    Result(403, reason)
   }
 
   notFound {
