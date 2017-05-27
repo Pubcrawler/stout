c402a19e0799287bcb78aba0e41d774557d8a000
Håkon Åmdal <hakon.amdal@schibsted.com>
2017-05-27 12:44:10 +0200

    Make stylecheck happy happy

--- src/main/scala/io/pubcrawler/stout/db/Types.scala
+++ src/main/scala/io/pubcrawler/stout/db/Types.scala
@@ -17,16 +17,20 @@ object Status extends Enumeration {
   val D = Value('D') // Declined
 }
 
-case class User(id: Option[Int], username: String, birthdate: Option[LocalDate], gender: Gender.Gender, email: String, facebookId: Int)
+case class User(id: Option[Int], username: String, birthdate: Option[LocalDate],
+                gender: Gender.Gender, email: String, facebookId: Int)
 
 case class Route(id: Option[Int], ownerId: Int)
 
-case class Crawl(id: Option[Int], title: String, ownerId: Int, routeId: Int, dateTime: LocalDateTime, address: String, city: String, lat: Double, lng: Double, radius: Float, description: String)
+case class Crawl(id: Option[Int], title: String, ownerId: Int, routeId: Int,
+                 dateTime: LocalDateTime, address: String, city: String,
+                 lat: Double, lng: Double, radius: Float, description: String)
 
 case class CrawlParticipant(crawlId: Int, userId: Int, status: Status.Status)
 
 case class RouteStop(routeId: Int, stopId: Int, order: Int)
 
-case class Stop(id: Option[Int], title: String, address: String, city: String, lat: Double, lng: Double)
+case class Stop(id: Option[Int], title: String, address: String, city: String,
+                lat: Double, lng: Double)
 
 case class Wish(userId: Int, stopId: Int)
